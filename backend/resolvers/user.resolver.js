8cd320a70f53e12bed18c71d3a2fde80d4b3dc85
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2024-12-28 15:53:48 +0530

    Passport configured

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -2,7 +2,7 @@ import { users } from "../dummyData/data.js";
 
 const userResolver = {
   Query: {
-    users: (_, _, { req, res }) => {
+    users: (_, { req, res }) => {
       return users;
     },
     user: (_, { userId }) => {
