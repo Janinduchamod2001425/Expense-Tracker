18f2680ce847591d5ea8e049351adcbd6ed64653
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2024-12-28 18:08:12 +0530

    user resolvers added

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,15 +1,101 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
-  Query: {
-    users: (_, { req, res }) => {
-      return users;
+  Mutation: {
+    signup: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fields are required.");
+        }
+
+        const existingUser = await User.findOne({ username });
+
+        if (existingUser) {
+          throw new Error("User already exists.");
+        }
+
+        // hashing password
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const boyProfilePic = `https://avatar.iran.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.iran.liara.run/public/girl?username=${username}`;
+
+        const newuser = await User({
+          username,
+          name,
+          password: hashedPassword,
+          gender,
+          profilePicture: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newuser.save();
+        await context.login(newuser);
+        return newuser;
+      } catch (error) {
+        console.log("Error in signUp: ", error);
+        throw new Error(error.message || "Internal server error");
+      }
     },
-    user: (_, { userId }) => {
-      return users.find((user) => user._id === userId);
+
+    login: async (_, { input }, context) => {
+      try {
+        const { username, password } = input;
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          password,
+        });
+
+        await context.login(user);
+        return user;
+      } catch (error) {
+        console.error("Error in login: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+
+    logout: async (_, __, context) => {
+      try {
+        await context.logout();
+        req.session.destroy((err) => {
+          if (err) throw err;
+        });
+
+        res.clearCookie("connect.sid");
+
+        return { message: "Logged out successfully" };
+      } catch (error) {
+        console.error("Error in logout: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
     },
   },
-  Mutation: {},
+
+  Query: {
+    authUser: async (_, __, context) => {
+      try {
+        const user = await context.getUser();
+        return user;
+      } catch (error) {
+        console.error("Error in authUser: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
+    },
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        return user;
+      } catch (error) {
+        console.error("Error in user query: ", error);
+        throw new Error(error.message || "Error getting user");
+      }
+    },
+  },
 };
 
 export default userResolver;
