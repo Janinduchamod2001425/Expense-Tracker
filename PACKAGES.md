26672fa7cb7bd575e30fa730575cf7dc8f54ffd3
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2025-01-10 10:30:33 +0530

    refetch queries fixed

--- PACKAGES.md
+++ PACKAGES.md
@@ -11,3 +11,9 @@ npm install express express-session graphql @apollo/server @graphql-tools/merge 
 ```bash
 npm install graphql @apollo/client react-router-dom react-icons react-hot-toast tailwind-merge @tailwindcss/aspect-ratio clsx chart.js react-chartjs-2 mini-svg-data-uri framer-motion
 ```
+
+### Add new Commits to Github Repo
+
+1. git add .
+2. git commit -m "refetch queries fixed"
+3. git push
--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,7 +1,10 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { GET_TRANSACTION } from "../graphql/queries/transaction.query";
+import {
+  GET_TRANSACTION,
+  GET_TRANSACTION_STATISTICS,
+} from "../graphql/queries/transaction.query";
 import { UPDATE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
 import toast from "react-hot-toast";
 
@@ -18,7 +21,7 @@ const TransactionPage = () => {
   const [updateTransaction, { loading: loadingUpdate }] = useMutation(
     UPDATE_TRANSACTION,
     {
-      refetchQueries: ["GetTransactions", "GetTransactionStatistics"],
+      refetchQueries: [{ query: GET_TRANSACTION_STATISTICS }],
     }
   );
 
